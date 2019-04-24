a9f077ab2fbebe08e6e86316a7c860d57b7719f6
tonyiovino <antonioiovino2004tony@gmail.com>
2019-04-24 13:24:04 +0000

    cubo

---
+++ cubo.py
@@ -0,0 +1,9 @@
+print("Scrivi il nunero del lato e ti calcolerò il volume del cubo!")
+
+num = input("Numero: ")
+num = int(num)
+
+volume = num * num * num
+
+print("Il volume di", num, "è", volume)
+
