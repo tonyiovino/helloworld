c6db9cab9be58b12fada8151c33cefac157ab67e
tonyiovino <antonioiovino2004tony@gmail.com>
2019-05-04 19:44:56 +0000

    primogrado

---
+++ primogrado.py
@@ -0,0 +1,17 @@
+print("EQUAZIONI PRIMO GRADO")
+print("ax + b = 0")
+
+A = input("A: ")
+A = int(A)
+B = input("B: ")
+B = int(B)
+
+if A==0 and B==0:
+    print("L'equazione 0 = 0 possono avere infinite soluzioni")
+
+elif A==0:
+    print("L'equazione", B, "= 0 non ha soluzioni")
+
+else:
+    x = B / A
+print("L'equazione ha soluzione:", x)
